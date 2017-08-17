a0905a350d7fbc766b7d18c7c8a36575cd556acb
Nir Weber <webernir@bina-fin.co.il>
2017-08-17 09:52:20 +0300

    added count members in/out

--- stats.js
+++ stats.js
@@ -13,7 +13,7 @@ function getCountByUser(lines) {
     .filter(line => line.substring(0, 5).includes('/'))
 
   let items = filtered.map((line, index) => ({
-    time: line.substring(0, line.indexOf(' - ')),
+    time: getTime(line),
     member: line.substring(line.indexOf('- ') + 2, line.indexOf(':', line.indexOf('- '))),
     index,
     line
@@ -28,8 +28,90 @@ function getCountByUser(lines) {
   return output
 }
 
-function get
+function getTime(line) {
+  return line.substring(0, line.indexOf(' - '))
+}
+
+function getAction(line) {
+  if (line.includes('joined')) {
+    return 'joined'
+  }
+  if (line.includes('added')) {
+    return 'added'
+  }
+  if (line.includes('left')) {
+    return 'left'
+  }
+  if (line.includes('removed')) {
+    return 'removed'
+  }
+  else {
+    return 'message'
+  }
+}
+
+function getMember(line) {
+  const action = getAction(line)
+  const stripped = line.substring(line.indexOf('- ') + 2)
+  switch (action) {
+    case 'joined':
+      return stripped.substring(0, stripped.indexOf('joined') - 1)
+    case 'added':
+      return stripped.substring(stripped.indexOf('added') + 'added '.length)
+    case 'left':
+      return stripped.substring(0, stripped.indexOf('left') - 1)
+    case 'removed':
+      return stripped.substring(stripped.indexOf('removed') + 'removed '.length)
+    default:
+      return undefined
+  }
+}
+
+function getActionValue(action) {
+  switch (action) {
+    case 'joined':
+      return 1
+    case 'added':
+      return 1
+    case 'left':
+      return -1
+    case 'removed':
+      return -1
+    default:
+      return 0
+  }
+}
+
+function getMemberInOut(lines) {
+  const filterInOut =
+    lines.filter(line => line.includes('removed') || line.includes('left') || line.includes('added') || line.includes('joined'))
+      .filter(line => !line.includes(':', line.indexOf(':') + 1))
+      .filter(line => line.substring(0, 5).includes('/'))
+
+  const mapped = filterInOut.map((line, index) => ({
+    time: getTime(line),
+    member: getMember(line),
+    action: getAction(line),
+    actionValue: getActionValue(getAction(line)),
+    index,
+    line
+  }))
+
+  const grouped = _.groupBy(mapped, 'member')
+
+  const summed = Object.keys(grouped).map(member => {
+    return {
+      member,
+      sum: _.sumBy(grouped[member], 'actionValue')
+    }
+  })
+
+  let memberInOutOutput = summed.map(item => `${item.member},${item.sum}\r\n`)
+  
+  return memberInOutOutput
+}
 
 module.exports = {
-  getCountByUser
+  getCountByUser,
+  getMemberInOut
 }
